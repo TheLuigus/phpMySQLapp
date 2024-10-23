b793ed060b45cd3881ce6ab14c4924033551ac03
John Spurgeon <vf-17@protonmail.com>
2024-10-23 09:39:00 -0500

    Change to env vars

--- src/movies/includes/movieDatabase.php
+++ src/movies/includes/movieDatabase.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "mysql";
-$username = "root";
-$password = "admin";
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
 $dbname = "moviedb";
 
 $conn = mysqli_connect($servername, $username, $password,$dbname );
